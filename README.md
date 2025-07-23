148b755877d3c81481930c10b1fea972034f483b
ahbarnett <alex.h.barnett@gmail.com>
2025-07-23 16:15:30 -0400

    make R=4 example; choose M>=K for meaningfull null vector

--- README.md
+++ README.md
@@ -6,30 +6,33 @@ Explore exponential fitting methods such as Prony's, ESPRIT, etc.
 
 ## Prony for real decaying exponentials with real coeffs
 
+We assume regular grid data.
+There is some uncertainty about which null vector is the best choice.
 In Julia, run `src/try_prony.jl`
 
 Results:
 
 ```
-Try Prony: true decay rates [1.0, 3.0, 5.0]
-  ampls [0.31552274336867586, -1.6267504320446404, -0.48993012686076465]
+Try Prony: true decay rates [1.0, 3.0, 5.0, 10.0]
+  ampls [0.31552274336867586, -1.6267504320446404, -0.48993012686076465, 0.9627677544110731]
 
-n=10, K=6, M=5.          Hankel sing vals:
-5-element Vector{Float64}:
- 3.1973381020805136
- 0.1168726624254744
- 0.0017417203911870487
- 1.577673443359995e-8
- 1.4943520497383594e-8
-found 3 +ve Re roots with fitted rates:
-  [0.999967977433918, 3.0000430528738176, 5.000050327410862]
-        max root error 5.03e-05
+n=11, K=6, M=6.          Hankel sing vals:
+6-element Vector{Float64}:
+ 2.3070636723857008
+ 0.3176190380197283
+ 0.03923097422001804
+ 7.408266604575083e-5
+ 3.385056490482868e-8
+ 9.40134784848539e-9
+found 4 +ve Re roots with fitted rates:
+  [0.9997842638296758, 3.0006634893320516, 5.0024446023333535, 9.99987806580621]
+        max root rel error 0.000489
 fitted amplitudes:
-  [0.315504356874623, -1.6267663498344827, -0.48989580388149906]
-        cond(V)=33.4,   max resid 7.59e-09
+  [0.3153747092755308, -1.6275144448750392, -0.4890860658079341, 0.962835731178914]
+        cond(V)=102,    max resid 1.7e-08
 ```
 
-<img src="pics/try_prony_R3.png">
+<img src="pics/try_prony_R4.png">
 
 
 
--- src/try_prony.jl
+++ src/try_prony.jl
@@ -1,5 +1,5 @@
 # try Prony's method for real combo of real decaying exponentials.
-# Barnett 7/21/25
+# Barnett 7/21/25. Tweak 7/23/25 for meaningful null vector.
 # Ref: T. Sauer, Oberwolfach Snapshot 2018-004-EN
 # Prony's method: an old trick for new problems
 # https://publications.mfo.de/handle/mfo/1338
@@ -8,10 +8,10 @@ using GLMakie, LinearAlgebra, PolynomialRoots, Printf, Random
 verb = 1
 
 # make data = y values on grid tt
-h = 0.1    # spacing of data in t
-n = 10       # samples
+n = 11            # samples
+h = 1.0/(n-1)     # spacing of data in t, do last sample at t=1
 tt = (0:n-1)*h    # reg time sample grid
-re = [1.0, 3.0, 5.0]   # exp decay rates ("e" denotes exact)
+re = [1.0, 3.0, 5.0, 10.0]   # exp decay rates ("e" denotes exact)
 R = length(re)   # num terms (rates)
 rng = Xoshiro(43);   # fix seed
 ae = randn(rng, R)  # reproducible real amplitudes
@@ -22,28 +22,32 @@ noi = 1e-8
 y .+= noi*randn(rng, n)   # add noise
 
 # Prony step 1: extract decay rates
-K = 6  # num coefs in poly p, ie deg(p)-1, must be >R
+K = 6  # num coefs in poly p, ie deg(p)-1, must be >R, and see next...
+if 2K-1>n @warn "num conditions M does not exceed filter length K!"; end
 M = n-K+1   # resulting num of annihilation conditions
-if M<=R @warn "num conditions does not exceed R!"; end
 H = [y[i+j-1] for i=1:M, j=1:K]   # Hankel matrix
 U,S,Vt = svd(H; full=true)        # also get full nullspace in Vt
 @printf "\nn=%d, K=%d, M=%d. \t Hankel sing vals:\n" n K M
 display(S)
-trunc = 1e2*noi    # sing val cutoff
+trunc = 1e1*noi    # sing val cutoff
 ii = findall(S .< trunc)
-if isempty(ii) @warn @sprintf("no sing vals below trunc=%.3g",trunc); end
-i = ii[1]  #min(M,K)    # col index of V to use to get a R sing vec
+if isempty(ii)
+	@warn @sprintf("no sing vals below trunc=%.3g; using last one",trunc)
+	i = K    # take smallest sing vec (recall non-fat matrix M>=K)
+else
+	i = ii[1]  #min(M,K)    # col index of V to use to get a R sing vec
+end
 # unclear whether to use first sing val below trunc, or dip into true nullspace?
-# i = K   # ?
 v = Vt[:,i]    # R sing vec
 z = roots(v)      # the nonlin step
-zj = z[findall(real(z)>0 && real(z)<1 && abs(imag(z)) < 0.1 for z in z)]  # find roots nr [0,1]
-zj = real.(zj)       # enforce reality
+zj = z[findall(real(z)>0.2 && real(z)<1 && abs(imag(z)) < 0.1 for z in z)]  # roots nr [0.2,1]
+zj = real.(zj)       # enforce reality, nondecreasing
 rj = -log.(zj)/h    # extracted rates
+rj = sort(rj)
 nrj = length(rj)
 println("found ",nrj," +ve Re roots with fitted rates:\n  ",rj)
 if nrj==R
-	@printf "\tmax root error %.3g\n" norm(rj - re, Inf)
+	@printf "\tmax root rel error %.3g\n" norm(rj./sort(re) .- 1.0, Inf)
 end
 # Prony step 2: lin solve for coeffs given known rates
 V = [exp(-r*t) for t in tt, r in rj]   # Vandermonde matrix
